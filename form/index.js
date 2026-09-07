2c648ca96353173152d33b7ed80d04c15ee06b1d
Mariana Pondé <marianahponde@gmail.com>
2026-09-07 03:01:40 -0300

    Update index.js

--- form/index.js
+++ form/index.js
@@ -44,3 +44,50 @@ sign_in_btn.addEventListener('click', () => {
 
 
 // - - - VALIDAÇÃO - - -
+
+const dados = {
+  usuario: document.querySelector("input[placeholder='Nome de Usuário']").value,
+  email: document.querySelector("input[placeholder='Email']").value,
+  telefone: document.querySelector("input[placeholder='Número de Telefone']").value,
+  nascimento: document.querySelector("input[placeholder='Data de nascimento']").value,
+  senha: document.querySelector("input[placeholder='Senha']").value,
+  confirmacao: document.querySelector("input[placeholder='Confirmação de senha']").value
+};
+
+/*
+Fetch pega os valores digitados nos inputs do formulário e manda para o servidor.
+method: "POST", = Método que indica que estou mandando informações além de pedir
+Headers: = como os dados estão sendo enviados e o que o servidor deve esperar.
+body: = é o conteúdo que vai ser enviado.
+body: JSON.stringify(dados) = transforma o objeto JS em texto JSON para o PHP entender.
+*/
+fetch("processa.php", {
+  method: "POST",
+  headers: {
+    "Content-Type": "application/json" /*formato*/
+  },
+  body: JSON.stringify(dados)
+})
+
+/*
+.then(response => response.json()) = Quando o servidor responde, o JS pega a resposta e transforma em JSON.
+Ou seja, transforma o texto que o PHP devolveu (echo json_encode(...)) em um objeto JavaScript.
+
+.then(data => {
+  console.log("Resposta do servidor:", data);
+})
+Depois que a resposta foi convertida em JSON, ela cai aqui como data.
+Usa esse data para mostrar mensagens na tela, atualizar o layout, etc
+
+.catch(error => console.error("Erro:", error))
+Se acontecer algum problema (servidor fora do ar, erro de rede, JSON inválido), o código cai aqui.
+O catch serve para tratar erros e evitar que o programa quebre.
+Nesse caso, ele mostra no console:
+“Erro: error”
+error serve pra mostrar o motivo do problema
+*/
+.then(response => response.json())
+.then(data => {
+  console.log("Resposta do servidor:", data);
+})
+.catch(error => console.error("Erro:", error))
